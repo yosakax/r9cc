48d39526d145299d358121036e14beeaf55e9f6f
yosaka <fallingfluit@gmail.com>
2024-04-07 02:11:39 +0900

    make 1 integer compiled compiler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Invalid arguments number.");
+    }
+
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!("  mov rax, {}", args[1]);
+    println!("  ret");
 }
